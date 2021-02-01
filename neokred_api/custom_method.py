540cea7f1374747bb8c4a4384c89d7b6aa572d2d
Neha0037 <ntalokar@yahoo.com>
2021-02-01 17:09:17 +0530

    update2

--- neokred_api/custom_method.py
+++ neokred_api/custom_method.py
@@ -32,3 +32,34 @@ def get_neo_activate_card(email,password,api_key,client_hash_id,base_url):
 		return response.json()['details']['uatUrl']
 	except HTTPError as http_err:
 		frappe.throw(_("HTTP Error {0}".format(http_err)))
+
+
+@frappe.whitelist()
+def get_neo_check_card_status(email,password,api_key,client_hash_id,base_url,mobile_number):
+	def get_neokred_token():
+		headers = {
+			'content-type':'application/json'
+		}
+		body = {
+			'email':email,
+			'password': password
+		}
+		try:
+			response = requests.post('https://preprod.bankx.money:9091/api/v1/authenticate',headers=headers,data=json.dumps(body))
+			token = response.json()['token']
+			return token
+		except HTTPError as http_err:
+			frappe.throw(_("HTTP Error {0}".format(http_err)))
+
+	neo_kred_token = 'Bearer ' + get_neokred_token()
+	headers = {
+		'Authorization':  neo_kred_token,
+		'client-hash-id': client_hash_id,
+		'content-type': 'application/json'
+	}
+	data = { "mobileNumber": mobile_number }
+	try:
+		response = requests.post('https://preprod.bankx.money:9094/yespay/api/v1/card/capture',headers=headers,data=json.dumps(data))
+		return response.json()
+	except HTTPError as http_err:
+		frappe.throw(_("HTTP Error {0}".format(http_err)))
--- neokred_api/neokred_api/doctype/neokred_settings/neokred_settings.js
+++ neokred_api/neokred_api/doctype/neokred_settings/neokred_settings.js
@@ -5,6 +5,8 @@ frappe.ui.form.on('Neokred Settings', {
 	refresh: function(frm) {
 		frm.add_custom_button(__('Call API'), function() {
         frm.trigger("call_api");}, __("Make"));
+        frm.add_custom_button(__('Check Status'), function() {
+        frm.trigger("check_status");}, __("Make"));
     },
     call_api: function(frm) {
         frappe.call({
@@ -12,6 +14,15 @@ frappe.ui.form.on('Neokred Settings', {
             args: { email: frm.doc.email, password: frm.doc.password, api_key: frm.doc.api_key, client_hash_id: frm.doc.client_hash_id, base_url: frm.doc.base_url},
             callback: function(r) {
                 window.open(r.message);
+            }
+        });
+	},
+	check_status: function(frm) {
+        frappe.call({
+            method: "neokred_api.custom_method.get_neo_check_card_status",
+            args: { email: frm.doc.email, password: frm.doc.password, api_key: frm.doc.api_key, client_hash_id: frm.doc.client_hash_id, base_url: frm.doc.base_url, mobile_number: frm.doc.mobile_number},
+            callback: function(r) {
+            	console.log(r.message.details);
             }
         });
 	}
